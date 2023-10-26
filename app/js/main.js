db2553160b1e8969982fa8772fb6ccdfa91efa95
10hokageee <nikita.serdyukov.20033002@gmail.com>
2023-10-27 01:31:52 +0300

    added top slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function(){
 
+$(".promo__inner").slick({
+dots: true,
+autoplay: true,
+autoplaySpeed: 2000,
+nextArrow: '<button type="button" class="slick-next"><svg><use xlink:href="images/sprite.svg#prev-arrow"></use></svg></button>',
+prevArrow: '<button type="button" class="slick-prev"><svg><use xlink:href="images/sprite.svg#next-arrow"></use></svg></button>'
+});
 });
