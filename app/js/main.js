2bc1ba0761b789a2ac809e7ed7e4318ecb16f4af
10hokageee <nikita.serdyukov.20033002@gmail.com>
2023-10-30 23:00:13 +0200

    added filters

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,28 @@ fade: true,
 nextArrow: '<button type="button" class="slick-next"><svg><use xlink:href="images/sprite.svg#prev-arrow"></use></svg></button>',
 prevArrow: '<button type="button" class="slick-prev"><svg><use xlink:href="images/sprite.svg#next-arrow"></use></svg></button>'
 });
+
+$(".filter-range__input").ionRangeSlider({
+  type: "double",
+  min: 0,
+  max: 1000,
+  from: 200,
+  to: 800,
+  prefix: "$",
+
+  onStart: function (data) {
+    $(".filter-range__from").text(data.from);
+    $(".filter-range__to").text(data.to);
+  },
+
+  onChange: function (data) {
+     $(".filter-range__from").text(data.from);
+     $(".filter-range__to").text(data.to);
+     
+    
+},
+
+
 });
+});
+
